390692a63e0496845f183061faa11177980b9c6e
Keiran Hazell <Keiran.hazell@gmail.com>
2024-06-05 11:21:30 +0100

    board update

--- skeleton/board.ts
+++ skeleton/board.ts
@@ -5,9 +5,10 @@ import { crossText, noughtText } from "./academy"
 export type Player = 'nought' | 'cross' | 'nobody' | undefined
 export type Cell = number | Player | null
 
+
 // Return the current board state with either a noughtText or a crossText in
 // each position. Put a null in a position that hasn't been played yet.
-export function getBoard(): Cell[][] { 
+export function getBoard(): Cell[][] {
   console.log('getBoard was called')
   return []
 }
